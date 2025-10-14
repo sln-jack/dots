e3a199d1d48484b1e72303ba4fadcd8e91ba264d
Jack Foltz <jack@foltz.io>
2025-10-14 13:51:40 -0400

    better bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,30 @@
 set -euo pipefail
 cd $(dirname $0)
 
-MACOS=false
-[[ "$(uname -s)" == "Darwin" ]] && MACOS=true
+#------ Probe ------------------------------------------------------------------------------------------------
+echo "Probing environment..."
 
-# Bootstrap
+HOST=$(hostname)
+LINUX=false
+MACOS=false
+NIX=false
+[[ "$(uname -s)" == "Darwin" ]] && MACOS=true
+[[ "$(uname -s)" == "Linux" ]]  && LINUX=true
+[[ -x "$(command -v nix)" ]] && NIX=true
+
+echo "  Host: $HOST"
+$MACOS && echo "  OS: MacOS"
+$LINUX && echo "  OS: Linux"
+$NIX && echo "  Packages: Nix"
+
+#------ Bootstrap --------------------------------------------------------------------------------------------
 if $MACOS; then
     # Setup nix
     if ! command -v nix >/dev/null; then
         curl -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm --no-modify-profile
         . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
     fi
+    NIX=true
 
     # Setup homebrew
     if ! command -v brew >/dev/null; then
@@ -42,33 +56,39 @@ if $MACOS; then
         chsh -s "$HOME/.nix-profile/bin/fish"
     fi
 fi
+if $NIX; then
+    # Rust
+    rustup default | grep -q nightly || rustup default nightly
+fi
 
-# Rust
-rustup default | grep -q nightly || rustup default nightly
+#------ Configs ----------------------------------------------------------------------------------------------
+echo -e "\nSymlinking configs..."
 
 # Install config symlinks
 install() {
     SRC=$1
     DST=~/.config/$2
-    echo "$SRC -> $DST"
+    echo "  $SRC -> $DST"
     mkdir -p $(dirname $DST)
     ln -sf $PWD/config/$SRC $DST
 }
 
-install ghostty.conf ghostty/config
-install fish.fish fish/config.fish
 install tmux.conf tmux/tmux.conf
 install gitconfig git/config
 install gitignore git/ignore
-install direnv.toml direnv.toml
-install starship.toml starship.toml
-install neovide.toml neovide/config.toml
 install nvim/init.lua nvim/init.lua
 install nvim/lib.lua nvim/lua/lib.lua
 
 if $MACOS; then
     install fish_macos.fish fish/conf.d/macos.fish
+    install ghostty.conf ghostty/config
+    install neovide.toml neovide/config.toml
     install aerospace.toml aerospace/aerospace.toml
 fi
+if $NIX; then
+    install fish.fish fish/config.fish
+    install direnv.toml direnv.toml
+    install starship.toml starship.toml
+fi
 
-echo "✅ dotfiles installed!"
+echo -e "\n✅ Dots installed!"
