a665389698e0ec0f11c8b37e2c7f12f957f89d28
Jack Foltz <jack@selinicapital.com>
2025-12-12 13:17:16 -0500

    fish: SSH agent

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -121,3 +121,11 @@ function fish_user_key_bindings
     # Colon: expand abbrs like space
     bind ':' 'commandline -f expand-abbr; commandline -i :'
 end
+
+# Start SSH agent
+if test -z (pgrep ssh-agent)
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
