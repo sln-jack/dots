943fc4d8c14f67370212c96018583331e6f9a182
Jack Foltz <jack@selinicapital.com>
2026-02-18 15:33:12 -0500

    xinput: disable vblank

--- files/xinput.sh
+++ files/xinput.sh
@@ -5,3 +5,5 @@ fc-cache -fv $XDG_DATA_HOME/fonts & disown
 MOUSE=$(xinput | grep "Logitech USB Receiver Mouse" | awk -F'=' '{print $2}' | awk '{print $1}')
 xinput set-prop "$MOUSE" "libinput Accel Profile Enabled" 0 1
 xset r rate 210 66
+xset -dpms
+xset s off
