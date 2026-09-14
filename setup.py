77285795f20e4d7a4767275da6b27ae458b858c6
Jack Foltz <jack@foltz.io>
2026-09-14 19:35:41 -0400

    packages: remove tcpreplay, vde2, runst, wayland

--- setup.py
+++ setup.py
@@ -533,16 +533,6 @@ def libpcap(d: Path, v: str):
     build_autotools(WORK/f'libpcap-{v}', d, '--disable-dbus', '--enable-shared', '--enable-static')
     sh(f'rm -rf {d}/share/man')
 
-@pkg(deps={'libpcap'})
-def tcpreplay(d: Path, v: str):
-    libpcap = PKGS/'libpcap'
-    extract(f'https://github.com/appneta/tcpreplay/releases/download/v{v}/tcpreplay-{v}.tar.xz', WORK)
-    build_autotools(
-        WORK/f'tcpreplay-{v}', d,
-        f'--with-libpcap={libpcap}',
-        f'CFLAGS="-I{libpcap}/include" LDFLAGS="-L{libpcap}/lib"',
-    )
-
 @pkg()
 def netcat(d: Path, v: str):
     extract(f'https://sourceforge.net/projects/netcat/files/netcat/{v}/netcat-{v}.tar.gz', WORK)
@@ -552,11 +542,6 @@ def netcat(d: Path, v: str):
 def iperf3(d: Path, v: str):
     extract(f'https://github.com/esnet/iperf/releases/download/{v}/iperf-{v}.tar.gz', WORK)
     build_autotools(WORK/f'iperf-{v}', d)
-
-@pkg(deps={'automake'})
-def vde2(d: Path, v: str):
-    extract(f'https://github.com/virtualsquare/vde-2/archive/refs/tags/v{v}.tar.gz', WORK)
-    build_automake(WORK/f'vde-2-{v}', d)
 
 @pkg(deps={'rust'})
 def gping(d: Path, v: str):
@@ -619,244 +604,6 @@ def is_interactive_ssh(d: Path, v: str):
 def dmenu(d: Path, v: str):
     extract(f'https://github.com/sln-jack/dmenu/archive/refs/heads/master.tar.gz', WORK)
     sh(f'rm -f {WORK}/dmenu-master/config.h && make -j{cpus} && DESTDIR={d} make install', cwd=WORK/'dmenu-master')
-
-# TODO this is bad
-@pkg(deps={'rust'})
-def runst(d: Path, v: str):
-    rust = PKGS/'rust'
-    vars = f'PATH="{rust}/bin:$PATH" RUSTUP_HOME={rust}/rustup CARGO_HOME={CACHE}/cargo PKG_CONFIG_PATH=/usr/lib64/pkgconfig'
-    src = Path.home()/'git/runst'
-    sh(f'{vars} cargo build --release', cwd=src)
-    sh(f'mkdir -p {d}/bin')
-    sh(f'cp {src}/target/release/runst {d}/bin/')
-
-@pkg(deps={'automake', 'pkgconfig'})
-def libevdev(d: Path, v: str):
-    pkgconfig = PKGS/'pkgconfig'
-    extract(f'https://www.freedesktop.org/software/libevdev/libevdev-{v}.tar.xz', WORK)
-    build_automake(
-        WORK/f'libevdev-{v}', d,
-        '--enable-static', '--disable-shared',
-        env=f'ACLOCAL_PATH="{pkgconfig}/share/aclocal"'
-    )
-
-@pkg(deps={'automake', 'pkgconfig'})
-def libmtdev(d: Path, v: str):
-    extract(f'https://bitmath.se/org/code/mtdev/mtdev-{v}.tar.gz', WORK)
-    build_automake(
-        WORK/f'mtdev-{v}', d,
-        '--enable-static', '--disable-shared'
-    )
-
-@pkg(deps={'meson', 'ninja', 'libevdev', 'libmtdev'})
-def libinput(d: Path, v: str):
-    # WARNING: impure to take host libsystemd
-    system = '/usr/share/pkgconfig:/usr/lib64/pkgconfig'
-    libevdev = PKGS/'libevdev/lib/pkgconfig'
-    libmtdev = PKGS/'libmtdev/lib/pkgconfig'
-    extract(f'https://gitlab.freedesktop.org/libinput/libinput/-/archive/{v}/libinput-{v}.tar.gz', WORK)
-    build_meson(
-        WORK/f'libinput-{v}', d,
-        '-Ddocumentation=false',
-        '-Dtests=false',
-        '-Ddebug-gui=false',
-        '-Dlibwacom=false',
-        env=f'PKG_CONFIG_PATH="{libevdev}:{libmtdev}:{system}"'
-    )
-
-@pkg(deps={'clang'})
-def pugixml(d: Path, v: str):
-    extract(f'https://github.com/zeux/pugixml/archive/refs/tags/v{v}.tar.gz', WORK)
-    build_cmake(
-        WORK/f'pugixml-{v}', d,
-        '-DCMAKE_CXX_FLAGS="-stdlib=libc++"',
-    )
-
-@pkg(deps={'clang'})
-def libseat(d: Path, v: str):
-    # WARNING: impure to take host libsystemd
-    system = '/usr/share/pkgconfig:/usr/lib64/pkgconfig'
-    libinput = PKGS/f'libinput/lib64/pkgconfig'
-
-    extract(f'https://git.sr.ht/~kennylevinsen/seatd/archive/{v}.tar.gz', WORK)
-    build_meson(
-        WORK/f'seatd-{v}', d,
-        '-Dlibseat-seatd=disabled',
-        '-Dserver=disabled',
-        env=f'PKG_CONFIG_PATH="{system}:{libinput}"',
-    )
-
-@pkg(deps={'clang', 'pugixml'})
-def hyprwayland_scanner(d: Path, v: str):
-    pugixml = PKGS/'pugixml'
-    vars = f'PKG_CONFIG_PATH="{pugixml}/lib64/pkgconfig"'
-
-    extract(f'https://github.com/hyprwm/hyprwayland-scanner/archive/refs/tags/v{v}.tar.gz', WORK)
-    build_cmake(
-        WORK/f'hyprwayland-scanner-{v}', d,
-        '-DCMAKE_CXX_FLAGS="-stdlib=libc++"',
-        env=vars)
-
-
-@pkg(deps={'automake'})
-def libffi(d: Path, v: str):
-    extract(f'https://github.com/libffi/libffi/releases/download/v{v}/libffi-{v}.tar.gz', WORK)
-    build_autotools(WORK/f'libffi-{v}', d)
-
-#@pkg(deps={'cmake'})
-#def libexpat(d: Path, v: str):
-#    extract(f'https://github.com/libexpat/libexpat/releases/download/R_{v.replace('.', '_')}/expat-{v}.tar.gz', WORK)
-#    build_cmake(WORK/f'expat-{v}', d)
-
-@pkg(deps={'meson', 'libffi', 'libexpat', 'libxml2'})
-def wayland(d: Path, v: str):
-    libffi = PKGS/'libffi/lib/pkgconfig'
-    libexpat = PKGS/'libexpat/lib64/pkgconfig'
-
-    extract(f'https://gitlab.freedesktop.org/wayland/wayland/-/archive/{v}/wayland-{v}.tar.gz', WORK)
-    build_meson(
-        WORK/f'wayland-{v}', d,
-        '-Dscanner=true',
-        '-Dtests=false',
-        '-Ddocumentation=false',
-        '-Ddtd_validation=false',
-        env=f'PKG_CONFIG_PATH="{libffi}:{libexpat}"',
-    )
-
-@pkg(deps={'meson'})
-def wayland_protocols(d: Path, v: str):
-    wayland = PKGS/'wayland/lib64/pkgconfig'
-
-    extract(f'https://gitlab.freedesktop.org/wayland/wayland-protocols/-/archive/{v}/wayland-protocols-{v}.tar.gz', WORK)
-    build_meson(
-        WORK/f'wayland-protocols-{v}', d,
-        '-Dtests=false',
-        env=f'PKG_CONFIG_PATH="{wayland}"',
-    )
-
-@pkg(deps={'clang', 'pixman'})
-def hyprutils(d: Path, v: str):
-    pixman = PKGS/'pixman/lib64/pkgconfig'
-
-    extract(f'https://github.com/hyprwm/hyprutils/archive/refs/tags/v0.10.4.tar.gz', WORK)
-    build_cmake(
-        WORK/f'hyprutils-{v}', d,
-        '-DCMAKE_CXX_FLAGS="-stdlib=libc++"',
-        env=f'PKG_CONFIG_PATH="{pixman}"',
-    )
-
-@pkg(deps={'meson'})
-def pixman(d: Path, v: str):
-    extract(f'https://cairographics.org/releases/pixman-{v}.tar.gz', WORK)
-    build_meson(WORK/f'pixman-{v}', d)
-
-@pkg(deps={'meson'})
-def libdisplay_info(d: Path, v: str):
-    extract(f'https://gitlab.freedesktop.org/emersion/libdisplay-info/-/archive/{v}/libdisplay-info-{v}.tar.gz', WORK)
-    build_meson(WORK/f'libdisplay-info-{v}', d)
-
-@pkg()
-def hwdata(d: Path, v: str):
-    extract(f'https://github.com/vcrhonek/hwdata/archive/refs/tags/v{v}.tar.gz', WORK)
-    build_autotools(WORK/f'hwdata-{v}', d)
-
-@pkg(deps={'hyprutils'})
-def hyprlang(d: Path, v: str):
-    hyprutils = PKGS/'hyprutils/lib64/pkgconfig'
-
-    extract(f'https://github.com/hyprwm/hyprlang/archive/refs/tags/v{v}.tar.gz', WORK)
-    build_cmake(
-        WORK/f'hyprlang-{v}', d,
-        '-DCMAKE_CXX_FLAGS="-stdlib=libc++"',
-        env=f'PKG_CONFIG_PATH="{hyprutils}"',
-    )
-
-@pkg()
-def libzip(d: Path, v: str):
-    extract(f'https://github.com/nih-at/libzip/archive/refs/tags/v{v}.tar.gz', WORK)
-    build_cmake(
-        WORK/f'libzip-{v}', d,
-        '-DCMAKE_CXX_FLAGS="-stdlib=libc++"',
-    )
-
-@pkg()
-def pcre2(d: Path, v: str):
-    extract(f'https://github.com/PCRE2Project/pcre2/archive/refs/tags/pcre2-{v}.tar.gz', WORK)
-    build_automake(WORK/f'pcre2-pcre2-{v}', d)
-
-@pkg(deps={'pcre2'})
-def glib(d: Path, v: str):
-    src = WORK/f'glib-{v}'
-
-    pcre2 = PKGS/'pcre2/lib/pkgconfig'
-    sh(f'rm -r {src}/subprojects/gvdb')
-    sh(f'git clone https://gitlab.gnome.org/GNOME/gvdb --depth 1 --branch 2b42fc75f09dbe1cd1057580b5782b08f2dcb400 {src}/subprojects/gvdb')
-
-    extract(f'https://gitlab.gnome.org/GNOME/glib/-/archive/{v}/glib-{v}.tar.gz', WORK)
-    build_meson(
-        src, d,
-        '-Dwrap_mode=nodownload',
-        env=f'PKG_CONFIG_PATH="{pcre2}"',
-    )
-
-@pkg(deps={'glib'})
-def cairo(d: Path, v: str):
-    extract(f'https://cairographics.org/releases/cairo-{v}.tar.xz', WORK)
-    build_meson(
-        WORK/f'cairo-{v}', d,
-        '-Dwrap_mode=nodownload',
-    )
-
-@pkg(deps={'hyprlang', 'libzip', 'cairo'})
-def hyprcursor(d: Path, v: str):
-    hyprlang = PKGS/'hyprlang/lib64/pkgconfig'
-    libzip = PKGS/'libzip/lib64/pkgconfig'
-    cairo = PKGS/'cairo/lib64/pkgconfig'
-
-    extract(f'https://github.com/hyprwm/hyprcursor/archive/refs/tags/v{v}.tar.gz', WORK)
-    build_cmake(
-        WORK/f'hyprcursor-{v}', d,
-        '-DCMAKE_CXX_FLAGS="-stdlib=libc++"',
-        env=f'PKG_CONFIG_PATH="{hyprlang}:{libzip}"',
-    )
-
-@pkg(deps={'meson', 'hyprwayland_scanner', 'libinput', 'libseat', 'wayland', 'wayland_protocols', 'hyprutils'})
-def aquamarine(d: Path, v: str):
-    clang = PKGS/'clang/lib/x86_64-unknown-linux-gnu'
-    hyprwayland_scanner = PKGS/'hyprwayland_scanner'
-    libinput = PKGS/'libinput/lib64/pkgconfig'
-    libseat = PKGS/'libseat/lib64/pkgconfig'
-    wayland = PKGS/'wayland/lib64/pkgconfig'
-    wayland_protocols = PKGS/'wayland_protocols/share/pkgconfig'
-    hyprutils = PKGS/'hyprutils/lib64/pkgconfig'
-    pixman = PKGS/'pixman/lib64/pkgconfig'
-    libdisplay_info = PKGS/'libdisplay_info/lib64/pkgconfig'
-    hwdata = PKGS/'hwdata/share/pkgconfig'
-
-    extract(f'https://github.com/hyprwm/aquamarine/archive/refs/tags/v{v}.tar.gz', WORK)
-    build_cmake(
-        WORK/f'aquamarine-{v}', d,
-        f'-DCMAKE_PREFIX_PATH={hyprwayland_scanner}',
-        env=f'LD_LIBRARY_PATH="{clang}" PKG_CONFIG_PATH="{libseat}:{libinput}:{wayland}:{wayland_protocols}:{hyprutils}:{pixman}:{libdisplay_info}:{hwdata}"',
-    )
-
-@pkg(deps={'cmake', 'wayland', 'wayland_protocols', 'udis86', 'aquamarine', 'hyprlang', 'hyprcursor'})
-def hyprland(d: Path, v: str):
-    src = WORK/f'Hyprland-{v}'
-
-    wayland = PKGS/'wayland/share/pkgconfig'
-    wayland_protocols = PKGS/'wayland_protocols/share/pkgconfig'
-    aquamarine = PKGS/'aquamarine/lib64/pkgconfig'
-    hyprlang = PKGS/'hyprlang/lib64/pkgconfig'
-
-    # udis86 submodule
-    extract(f'https://github.com/canihavesomecoffee/udis86/archive/master.tar.gz', WORK)
-    sh(f'rm -rf {src}/subprojects/udis86 && mv {WORK}/udis86-master {src}/subprojects/udis86')
-
-    build_cmake(
-        src, d,
-        env=f'PKG_CONFIG_PATH="{wayland}:{wayland_protocols}:{aquamarine}:{hyprlang}"',
-    )
 
 # --- run ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
@@ -933,8 +680,6 @@ if __name__ == '__main__':
     libpcap('1.10.5')
     netcat('0.7.1')
     iperf3('3.21')
-    tcpreplay('4.5.1')
-    if sys == 'linux': vde2('2.3.3')
     gping('1.20.1')
     oha('1.12.1')
     snitch('0.2.2')
@@ -949,33 +694,8 @@ if __name__ == '__main__':
         # X11 Windowing
         dwm('6.6')
         dmenu('5.4')
-        runst('0.2.0')
         sshot('1.0')
         scast('1.0')
-
-    # Wayland Windowing
-    # libevdev('1.12.1')
-    # libmtdev('1.1.7')
-    # libinput('1.30.0')
-    # libseat('0.9.1')
-    # libffi('3.5.2')
-    # libexpat('2.7.3')
-    # wayland('1.24.0')
-    # wayland_protocols('1.46')
-    # pixman('0.46.4')
-    # hwdata('0.402')
-    # libdisplay_info('0.3.0')
-    # hyprutils('0.10.4')
-    # pugixml('1.15')
-    # hyprwayland_scanner('0.4.5')
-    # aquamarine('0.10.0')
-    # hyprlang('0.6.7')
-    # libzip('1.11.4')
-    # pcre2('10.47')
-    # glib('2.86.2')
-    # cairo('1.18.4')
-    # hyprcursor('0.1.13')
-    # hyprland('0.52.2')
 
     # Load extensions
     ext = ROOT/'setup.d'
@@ -1059,7 +779,6 @@ if __name__ == '__main__':
     conf('starship.toml')
     conf('alacritty.toml')
     conf('neovide.toml', 'neovide/config.toml')
-    conf('runst/runst.toml')
     conf('podman-storage.conf', 'containers/storage.conf')
     if sys == 'linux':
         conf('fish/linux.fish', 'fish/conf.d/linux.fish')
